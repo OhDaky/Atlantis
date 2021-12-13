f261b1bf8237c3f33571bd57e073ccdd62624134
OhDaky <ttakko88@gmail.com>
2021-12-13 16:58:53 +0900

    🍊 메인 페이지 구조

--- client/src/Pages/MainPage.js
+++ client/src/Pages/MainPage.js
@@ -1,4 +1,6 @@
 import React, { useState, useEffect } from "react";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faBars } from "@fortawesome/free-solid-svg-icons";
 
 import "./Style/MainPage.css";
 import "../Components/Style/bootstrap.css";
@@ -12,9 +14,19 @@ export default function MainPage() {
   
 
   return (
-    <div>
-      <Navigation />
-      <p>MainPage</p>
+    <div className="main-page">
+      {/* <Navigation /> */}
+      <div>
+        <div className="main-title">Atlantis</div>
+        <FontAwesomeIcon icon={faBars} className="main-hamburger" />
+        <div className="main-nav">main</div>
+      </div>
+      <div className='box'>
+        <div className='wave -one'></div>
+        <div className='wave -two'></div>
+        <div className='wave -three'></div>
+      </div>
+      <a className="main-to-home" href="../">To the Atlantis</a>
     </div>
   );
 }
