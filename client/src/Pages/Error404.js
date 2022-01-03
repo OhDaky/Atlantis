fb9313e2c3d836df7450b2f8ec08ece110122423
OhDaky <ttakko88@gmail.com>
2022-01-04 00:04:42 +0900

    💡 에러페이지 생성

--- client/src/Pages/Error404.js
+++ client/src/Pages/Error404.js
@@ -3,11 +3,17 @@ import React, { useState, useEffect } from "react";
 import "./Style/Error404.css";
 
 export default function Error404() {
-
+  // function goMain(e) {
+  //   window.location.href = ''
+  // }
 
   return (
-    <>
-      <p>Error 404</p>
-    </>
+    <div>
+      <div className="errorMessage">
+        <div className="error404">404</div>
+        <div className="page">잘못된 주소입니다!</div>
+        <div className="page">아래 페이지로 이동하세요</div>
+      </div>
+    </div>
   );
 }
